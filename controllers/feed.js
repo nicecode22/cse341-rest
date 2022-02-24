c9f179ab2b258c0640a131ddb96ca81b6cc0c7bc
nicecode22 <nicecode22@gmail.com>
2022-02-23 19:24:35 -0600

    commit

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,14 @@
-exports.getPost =(req, res, next) => {
-    
-}
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+        posts: [{title: 'First Post', content: 'This is the first post!'}]
+    });
+};
+
+exports.postPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    res.status(201).json({ //201 es para decir que se creo el recurso exitosamente
+        message: 'Post created!',
+        post: {id: new Date().toISOString(), title: title, content, content}
+    });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -1,12 +1,11 @@
 const express = require('express');
+
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
-router.get('/post', feedController.getPost);
+router.get('/posts', feedController.getPosts);
 
-
-
-
+router.post('/post', feedController.postPost);
 
 module.exports = router;
