ac67842e1b79bb87cb3f934e45842eb725072b89
nicecode22 <nicecode22@gmail.com>
2022-03-07 23:05:39 -0600

    commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -17,4 +18,8 @@ app.use((req, res, next) => {
 // GET /feed/post
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect('mongodb+srv://nicecode22:mmczaho947@cluster0.avaa3.mongodb.net/messages?retryWrites=true&w=majority')
+.then(result => {
+    app.listen(8080);
+})
+.catch(err => console.log(err));
