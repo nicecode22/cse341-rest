09f7158f59a01b602702836cf400c7f17a831b90
nicecode22 <nicecode22@gmail.com>
2022-03-08 00:18:51 -0600

    commit

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,19 +3,15 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: '1',
-                title: 'First Post', 
-                content: 'This is the first post!', 
-                imageUrl: 'images/depre.jpg',
-                creator: {
-                    name: 'Mell'
-                },
-                createdAt: new Date()
-            }
-        ]
+    Post.find()
+    .then(posts => {
+        res.status(200).json({message: 'Fetched posts successfully', posts: posts});
+    })
+    .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
     });
 };
 
@@ -39,6 +35,25 @@ exports.postPost = (req, res, next) => {
             message: 'Post created!',
             post: result
         });
+    })
+    .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post => {
+        if (!post) {
+            const error = new Error('Couldnt find post');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({message: 'Post fetched', post: post});
     })
     .catch(err => {
         if (!err.statusCode) {
--- routes/feed.js
+++ routes/feed.js
@@ -7,9 +7,19 @@ const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', [
-    body('title').trim().isLength({min: 7}),
-    body('content').trim().isLength({min: 5})
-], feedController.postPost);
+router.post(
+    '/post', 
+    [
+        body('title')
+            .trim()
+            .isLength({min: 7}),
+        body('content')
+            .trim()
+            .isLength({min: 5})
+    ], 
+    feedController.postPost
+);
+
+router.get('/post/:postId', feedController.getPost);
 
 module.exports = router;
