15058a574dc0862caeab777c11f0ced090afb919
nicecode22 <nicecode22@gmail.com>
2022-03-08 14:17:32 -0600

    commit

--- routes/feed.js
+++ routes/feed.js
@@ -12,7 +12,7 @@ router.post(
     [
         body('title')
             .trim()
-            .isLength({min: 7}),
+            .isLength({min: 5}),
         body('content')
             .trim()
             .isLength({min: 5})
@@ -21,5 +21,17 @@ router.post(
 );
 
 router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId', [
+    body('title')
+        .trim()
+        .isLength({min: 5}),
+    body('content')
+        .trim()
+        .isLength({min: 5})
+], feedController.updatePost
+);
+
+router.delete('/post/:postId', );
 
 module.exports = router;
