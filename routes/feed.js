5f6212e8d6732fabb0027b541bf5e07de329d142
nicecode22 <nicecode22@gmail.com>
2022-03-09 10:11:39 -0600

    commit

--- routes/feed.js
+++ routes/feed.js
@@ -32,6 +32,6 @@ router.put('/post/:postId', [
 ], feedController.updatePost
 );
 
-router.delete('/post/:postId', );
+router.delete('/post/:postId', feedController.deletePost);
 
 module.exports = router;
